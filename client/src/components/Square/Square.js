48fb1baebd36fb2b606122d50629a312ad84f50b
gdickey273 <gdickey273@gmail.com>
2020-12-04 13:09:41 -0500

    For each square, if there is a piece value in the boardState object, render Piece component and pass piece type and isWht in props. (changed from conditionally rendering a different component for each piece type)

--- client/src/components/Square/Square.js
+++ client/src/components/Square/Square.js
@@ -1,36 +1,20 @@
-import React from "react";
-import Pawn from "../Pawn";
-import Rook from "../Rook";
-import Knight from "../Knight";
-import Bishop from "../Bishop";
-import Queen from "../Queen";
-import King from "../King";
+import React, { useContext } from "react";
+import "./style.css";
+import Piece from "../Piece";
 
 function Square(props) {
-  const { squareColor, currentPiece } = props;
-
-  function getPieceComponent() {
-    switch (currentPiece.type) {
-      case "P" : 
-      return <Pawn color={currentPiece.color}/>;
-      case "R" :
-        return <Rook color={currentPiece.color}/>;
-      case "N" :
-        return <Knight color={currentPiece.color}/>;
-      case "B" : 
-        return <Bishop color={currentPiece.color}/>
-      case "Q" : 
-        return <Queen color={currentPiece.color}/>
-      case "K" :
-        return <King color={currentPiece.color}/>
-    }
-  }
+  const { sqrIsWht, name, boardState } = props;
+  //const boardState = useContext(PositionContext);
   
   return (
     <>
-    <div className={squareColor + "Square"} >
-      {getPieceComponent()}
+    {boardState && 
+    <div className={ sqrIsWht ? "whtSquare square" : "blkSquare square" } >
+      { boardState[name]?.piece &&
+        <Piece type={boardState[name].piece.type} isWht={boardState[name].piece.isWht} />
+      } 
     </div>
+    }
     </>
   )
 }
