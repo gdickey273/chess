fe5cf9e4a4aa817015c07164515f7a7cd13ca7e6
gdickey273 <gdickey273@gmail.com>
2020-12-05 10:27:31 -0500

    moved helper functions outside of export object

--- client/src/utils/MOVEMENT/shift.js
+++ client/src/utils/MOVEMENT/shift.js
@@ -1,18 +1,13 @@
 const rankString = "abcdefgh";
 function incrementRank(rank) {
-  
+  return rankString[rankString.indexOf(rank) + 1];
 }
 
 function decrementRank(rank) {
   return rankString[rankString.indexOf(rank) - 1];
 }
+
 const shift = {
-  incrementRank: function(rank) {
-    return rankString[rankString.indexOf(rank) + 1];
-  },
-  decrementRank: function(rank) {
-    return rankString[rankString.indexOf(rank) + 1];
-  },
   wht: function(sqrName) {
     return `${sqrName[0]}${parseInt(sqrName[1]) + 1}`
   },
@@ -20,10 +15,10 @@ const shift = {
     return `${sqrName[0]}${parseInt(sqrName[1]) - 1}`
   },
   alph: function(sqrName) {
-    return `${this.incrementRank(sqrName[0])}${sqrName[1]}`
+    return `${incrementRank(sqrName[0])}${sqrName[1]}`
   },
   unAlph: function(sqrName) {
-    return `${this.decrementRank(sqrName[0])}${sqrName[1]}`
+    return `${decrementRank(sqrName[0])}${sqrName[1]}`
   },
   whtAlph: function(sqrName) {
     return `${this.wht(this.alph(sqrName))}`
